6a17ab570262710d047f998c468b22a3ceee968c
joker8phoenix <joker.and.phoenix@gmail.com>
2021-08-19 16:06:33 +0900

    20210819

--- R/myGraph.R
+++ R/myGraph.R
@@ -19,45 +19,6 @@ mysave <- function(filename,w=pdf.sw,h=pdf.sh){
 }
 
 ############
-### size ###
-############
-sh <- 8.27*0.8
-sw <- sh*sqrt(2)
-gh <- sqrt((sqrt(10)-sqrt(2))/2)*sh
-gw <- gh*(1+sqrt(5))/2
-png.sh <- 320
-png.sw <- png.sh*sqrt(2)
-png.gh <- sqrt((sqrt(10)-sqrt(2))/2)*png.sh
-png.gw <- png.gh*(1+sqrt(5))/2
-pdf.sh <- 4.5*0.8
-pdf.sw <- pdf.sh*sqrt(2)
-pdf.gh <- sqrt((sqrt(10)-sqrt(2))/2)*pdf.sh
-pdf.gw <- pdf.gh*(1+sqrt(5))/2
-
-silver.h <- 8.27
-silver.w <- silver.h*sqrt(2)
-gold.h <- sqrt((sqrt(10)-sqrt(2))/2)*silver.h
-gold.w <- gold.h*(1+sqrt(5))/2
-
-##############
-### colour ###
-##############
-cCyan <- "#00a0e9"
-cyan <- "#00a0e9"
-cMagenta <- "#e4007f"
-magenta <- "#e4007f"
-cGreen <- "#009944"
-green <- "#009944"
-cOrange <- "#f39800"
-orange <- "#f39800"
-cLightBlue <- "#0068b7"
-lightblue <- "#0068b7"
-qcolours <- c(cCyan,cMagenta,cGreen,cLightBlue,cOrange)
-#scolours = colorRampPalette(c(cCyan,"grey60",cOrange))
-#jet.colors <- colorRampPalette(c("#00007F", "blue", "#007FFF", "cyan", "#7FFF7F", "yellow", "#FF7F00", "red", "#7F0000"))
-jet.colours <- c("#00007F", "blue", "#007FFF", "cyan", "#7FFF7F", "yellow", "#FF7F00", "red", "#7F0000")
-
-############
 ### grid ###
 ############
 # Multiple plot function
@@ -98,7 +59,3 @@ multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
     }
   }
 }
-
-
-
-
